c96e3f6216d301c98223c8db5d7a90ed1b9c27e0
Tim Bryant <tiomothybryant3@gmail.com>
2022-01-25 01:40:35 -0500

    fix start server  scipt

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-. /home/ec2-user/.bashrc && cd $HOME/csebot && pyenv exec pip install -r requirements.txt
+. /home/ec2-user/.bashrc && cd $HOME/cse-slackbot && pyenv exec pip install -r requirements.txt
